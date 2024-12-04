12d944a5b98e0df7227465e377aaaea0e6fe4536
amrabdalsattar <amrmmohamed00@gmail.com>
2024-12-04 07:34:37 +0200

    - edited app_router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -5,9 +5,6 @@ import 'routes.dart';
 
 class AppRouter {
   static Route generateRoute(RouteSettings settings) {
-    // This arguments to be passed in any screen like this (arguments as ClassName)
-    final arguments = settings.arguments;
-
     switch (settings.name) {
       case Routes.onBoarding:
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
