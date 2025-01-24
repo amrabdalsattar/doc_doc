d27a2329d4cea8d1d2e534a0cfab649ee30f48bc
amrabdalsattar <amrmmohamed00@gmail.com>
2025-01-24 04:29:51 +0200

    - finished login networking - finished login state management - added login model classes (Request & Response) - maintained error handler - added dio factory - added api service class - added api constants class - created ApiResult class to provide failure and success scenarios -  added Dependency Injection - added build_runner package

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,11 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../features/login/logic/cubit/login_cubit.dart';
 import '../../features/login/ui/login_screen.dart';
 import '../../features/onboarding/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../di/dependency_injection.dart';
 import 'routes.dart';
 
 class AppRouter {
@@ -11,7 +15,11 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const OnboardingScreen());
 
       case Routes.login:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => getIt<LoginCubit>(),
+                  child: const LoginScreen(),
+                ));
 
       default:
         return MaterialPageRoute(
